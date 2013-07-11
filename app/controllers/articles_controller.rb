56e82c7b3b5f58435694f1755d11e65380e99338
simmi.badhan <simmi@addvalsolutions.com>
2013-07-11 18:20:42 +0530

    form-based workflow feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,5 +6,31 @@ class ArticlesController < ApplicationController
   def show
   	@article = Article.find(params[:id])
   end
+  def new
+  	@article = Article.new
+  end
+  def create
+    @article = Article.new( title: params[:article][:title],
+                            body: params[:article][:body])                               
+    @article.save
+    flash.notice = "New Article '#{@article.title}' Created!"
+    redirect_to article_path(@article)
+  end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash.notice = "Article '#{@article.title}' Deleted!"
+    redirect_to articles_path
+  end
+  def edit
+    @article = Article.find(params[:id])  
+  end
+  def update
+    @article = Article.find(params[:id])
+    @article.update_attributes(title: params[:article][:title],
+                            body: params[:article][:body])  
+    flash.notice = "Article '#{@article.title}' Updated!"
+    redirect_to article_path(@article) 
+  end
 
 end
